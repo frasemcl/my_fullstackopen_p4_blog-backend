e6ad55a8ae7a978bd4652f58fee35a5325af3188
Fraser McLaughlin <fraser.mclaughlin@outlook.com>
2023-01-05 10:14:20 -0500

    completed ex4.2

---
+++ models/blog.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+const blogSchema = new mongoose.Schema({
+	title: String,
+	author: String,
+	url: String,
+	likes: Number,
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
